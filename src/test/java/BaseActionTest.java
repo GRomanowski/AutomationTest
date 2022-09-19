b8ee1e1ab8e9103bdd6841a0ce2dbf3bb9009adf
Gromanowski <grzegorz.romanowski@gmail.com>
2022-09-19 21:05:48 +0200

    7

--- src/test/java/BaseActionTest.java
+++ src/test/java/BaseActionTest.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,5 +14,9 @@ public class BaseActionTest {
         driver.get("https://testeroprogramowania.github.io/selenium/");
         driver.findElement(By.linkText("Podstawowa strona testowa")).click();
         driver.findElement(By.id("fname")).sendKeys("Adam");
+        driver.findElement(By.name("username")).sendKeys("AAA");
+        driver.findElement(By.name("username")).sendKeys(Keys.TAB);
+
+
     }
 }
