af38a5e1b26092e387cc8eb58121dfd45d713c72
Gromanowski <grzegorz.romanowski@gmail.com>
2022-09-26 13:20:31 +0200

    12

--- src/test/java/HomeWorkBasic.java
+++ src/test/java/HomeWorkBasic.java
@@ -7,35 +7,69 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class HomeWorkBasic {
 
+
     @Test
-    public void basicSiteTest() {
+    public void isNameisDisplayed() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
-//// Pobieranie nazwy h1
-//        WebElement h1Name = driver.findElement(By.xpath("/html/body/h1"));
-//        Assert.assertTrue(h1Name.getText().equals("Witaj na stronie testowej"));
-//// Klikanie na przycisk kliknij mnie
-//        Actions action = new Actions(driver);
-//        action.click(driver.findElement(By.id("clickOnMe"))).perform();
-//// Obsługa alertu
-//        Alert alert = driver.switchTo().alert();
-//        alert.accept();
-//// klikanie w link W3Schools
-//        driver.findElement(By.linkText("Visit W3Schools.com!")).click();
-// klikanie w link IamWeirdLink
-//        driver.findElement(By.linkText("IamWeirdLink")).click();
-//// Wyciaganie wartosci z tabeli
-//        System.out.println(driver.findElement(By.cssSelector("body > table > tbody > tr:nth-child(2) > td:nth-child(1)")).getText());
-//        System.out.println(driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]")).getText());
-// Wyciąganie wartości z Selectora
+        WebElement h1Name = driver.findElement(By.xpath("/html/body/h1"));
+        Assert.assertTrue(h1Name.getText().equals("Witaj na stronie testowej"));
+        driver.close();
+    }
 
+    @Test
+    public void clickOnMe() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        Actions action = new Actions(driver);
+        action.click(driver.findElement(By.id("clickOnMe"))).perform();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        driver.close();
+    }
 
+    @Test
+    public void clickOnFirstLink() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        driver.findElement(By.linkText("Visit W3Schools.com!")).click();
+        driver.close();
+    }
 
+    @Test
+    public void clickOnSecondtLink() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        driver.findElement(By.linkText("IamWeirdLink")).click();
+        driver.close();
+    }
 
-//        driver.close();
+    @Test
+    public void itemFromTable() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        System.out.println(driver.findElement(By.cssSelector("body > table > tbody > tr:nth-child(2) > td:nth-child(1)")).getText());
+        System.out.println(driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]")).getText());
+        driver.close();
+    }
+
+    @Test
+    public void select() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        WebElement selectCar = driver.findElement(By.cssSelector("select"));
+        Select cars = new Select(selectCar);
+        cars.selectByIndex(3);
+        driver.close();
     }
 }
