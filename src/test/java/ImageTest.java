284f943fe23d0809b3c334b6534d04467d41d38f
Gromanowski <grzegorz.romanowski@gmail.com>
2022-10-12 23:07:12 +0200

    TestNG cz2

--- src/test/java/ImageTest.java
+++ src/test/java/ImageTest.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.sql.SQLOutput;
+
 
 public class ImageTest {
 
     @Test
     public void isImageDisplayed(){
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
