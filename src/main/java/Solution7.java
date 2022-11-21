828d6634c6159df8b4fdf79a191c2a5c1322b04b
Gromanowski <grzegorz.romanowski@gmail.com>
2022-11-21 21:33:46 +0100

    25 cc

---
+++ src/main/java/Solution7.java
@@ -0,0 +1,11 @@
+public class Solution7 {
+    public final static int A = 5;
+    public final static int B = 2;
+    public static final int C = A * B;
+
+    public static void main(String[] args) {
+    }
+    public int getValue() {
+        return C;
+    }
+}
